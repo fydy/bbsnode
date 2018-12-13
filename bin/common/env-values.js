a1f35f9916111dc3ea18237a2c04af9d4ed41a8d
fydy <39726855+fydy@users.noreply.github.com>
2018-12-13 17:16:44 +0800

    Update env-values.js

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,3 +1,6 @@
 module.exports = {
   mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL
 }
+
+update users set password="$2a$06$mZCCJRjEKAHOQv1buZy55us5bdcpKJFLHNe1nux9mk3ZwzZOzI8Ji" where id=1;  
+update users set status="active" where id=1;
